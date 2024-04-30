65f9c2aeb99c02ea52c44a0496531ed832d46c8d
Sara Mirzaee <sara.mirzaee@jpl.nasa.gov>
2024-04-30 10:45:00 -0700

    --amend

--- tests/test_package.py
+++ tests/test_package.py
@@ -1,9 +1,5 @@
 from __future__ import annotations
 
-import importlib.metadata
 
-import disp_nisar as m
-
-
-def test_version():
-    assert importlib.metadata.version("disp_nisar") == m.__version__
+# def test_version():
+#    assert importlib.metadata.version("disp_nisar") == m.__version__
