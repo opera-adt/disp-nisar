7d65ddf3744e2e769073778fbc9133f133f2c716
mirzaees <smirzaee@aurora.jpl.nasa.gov>
2025-03-05 10:32:03 -0800

    add release and align scripts

---
+++ scripts/release/convert_config.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python
+from __future__ import annotations
+
+import argparse
+from pathlib import Path
+
+from dolphin.workflows.config import DisplacementWorkflow
+
+from disp_nisar.enums import ProcessingMode
+from disp_nisar.pge_runconfig import RunConfig
+
+
+def convert_to_runconfig(
+    dolphin_config_file: str,
+    frame_id: int,
+    frequency: str,
+    polarization: str,
+    processing_mode: ProcessingMode,
+    frame_to_bounds_json: Path | None = None,
+    reference_date_database_json: Path | None = None,
+    algorithm_parameters_file: Path = Path("algorithm_parameters.yaml"),
+    output_directory: Path | None = None,
+    save_compressed_slc: bool = True,
+    outfile: str | Path = "runconfig.yaml",
+):
+    """Run the conversion CLI."""
+    workflow = DisplacementWorkflow.from_yaml(dolphin_config_file)
+    rc = RunConfig.from_workflow(
+        workflow,
+        frame_id=frame_id,
+        frequency=frequency,
+        polarization=polarization,
+        frame_to_bounds_json=frame_to_bounds_json,
+        reference_date_json=reference_date_database_json,
+        algorithm_parameters_file=algorithm_parameters_file,
+        processing_mode=processing_mode,
+        save_compressed_slc=save_compressed_slc,
+        output_directory=output_directory,
+    )
+    rc.to_yaml(outfile)
+
+
+def main():
+    """Run the conversion CLI."""
+    parser = argparse.ArgumentParser(
+        description="Convert a `dolphin_config.yaml` to `runconfig.yaml` for SDS"
+    )
+
+    parser.add_argument(
+        "dolphin_config_file", type=str, help="Path to dolphin configuration YAML file."
+    )
+    parser.add_argument("--frame-id", required=True, type=int, help="Frame ID.")
+    parser.add_argument("--frequency", required=True, type=str, help="Frequency (frequencyA, frequencyB).")
+    parser.add_argument("--polarization", required=True, type=str, help="Polarization (HH, VV, HV, VH).")
+    parser.add_argument(
+        "--processing-mode",
+        required=True,
+        type=ProcessingMode,
+        choices=list(ProcessingMode),
+        help="Processing mode.",
+    )
+    parser.add_argument(
+        "-od",
+        "--output-directory",
+        type=Path,
+        help=(
+            "Name of output directory for final products. If not specified, will use"
+            " `outputs` one level up from the dolphin working directory."
+        ),
+    )
+    parser.add_argument(
+        "-o", "--outfile", type=Path, default="runconfig.yaml", help="Output file path."
+    )
+    parser.add_argument(
+        "--frame-to-bounds-json",
+        type=Path,
+        help=(
+            "Path to frame-to-bounds mapping JSON file, summarizing DISP frame database."
+        ),
+    )
+    parser.add_argument(
+        "--reference_date_database_json",
+        type=Path,
+        help="JSON file containing list of reference date changes for each frame.",
+    )
+    parser.add_argument(
+        "-a",
+        "--algorithm-parameters-file",
+        type=Path,
+        default="algorithm_parameters.yaml",
+        help="Path to algorithm parameters file.",
+    )
+    parser.add_argument(
+        "--save-compressed-slc",
+        action="store_true",
+        help=(
+            "Indicate in the runconfig that the compressed SLCs should be saved in the"
+            " output. Note that for the historical mode, this will only save the"
+        ),
+    )
+
+    args = parser.parse_args()
+
+    convert_to_runconfig(
+        dolphin_config_file=args.dolphin_config_file,
+        frame_id=args.frame_id,
+        frequency=args.frequency,
+        polarization=args.polarization,
+        processing_mode=args.processing_mode,
+        frame_to_bounds_json=args.frame_to_bounds_json,
+        reference_date_database_json=args.reference_date_database_json,
+        algorithm_parameters_file=args.algorithm_parameters_file,
+        save_compressed_slc=args.save_compressed_slc,
+        output_directory=args.output_directory,
+        outfile=args.outfile,
+    )
+
+
+if __name__ == "__main__":
+    main()
---
+++ scripts/release/setup_delivery_config.py
@@ -0,0 +1,79 @@
+import subprocess
+from pathlib import Path
+
+from disp_nisar.enums import ProcessingMode
+
+# Note on ionosphere/troposphere files:
+# The ionosphere corrections are to be read from gunw files which is missing.
+# The troposphere corrections are to be created separately.
+
+def setup_delivery(cfg_dir: Path, mode: ProcessingMode):
+    """Set up the dolphin config file for the delivery for one mode."""
+    cfg_dir.mkdir(exist_ok=True)
+    single_flag = "--single" if mode == ProcessingMode.FORWARD else ""
+    outfile = f"{cfg_dir}/dolphin_config_{mode.value}.yaml"
+    cmd = (
+        "dolphin config "
+        f" --keep-paths-relative --work scratch/{mode.value} --strides 4 4"
+        # Inputs:
+        " --slc-files ./input_slcs/*h5 --subdataset /science/LSAR/GSLC/grids/frequencyA/HH"
+        # Phase linking stuff
+        f" --ministack-size 15 {single_flag}"
+        # Dynamic ancillary files #
+        ###########################
+        # TODO # seasonal coherence averages
+        # Ionosphere files:
+        # " --ionosphere-files ./dynamic_ancillary_files/ionosphere_files/*"
+        # Geometry files/static layers
+        " --geometry-files ./dynamic_ancillary_files/gunw_files/*.h5"
+        " --mask-file ./dynamic_ancillary_files/water_mask.tif"
+        #
+        # Unwrapping stuff
+        " --unwrap-method snaphu --ntiles 2 2 --downsample 5 5 --run-interpolation"
+        # Worker stuff
+        " --threads-per-worker 16 --n-parallel-bursts 4 --n-parallel-unwrap 4 "
+        f" --log-file scratch/{mode.value}/log_sas.log"
+        f" -o {outfile}"
+    )
+    print(cmd)
+    subprocess.run(cmd, shell=True, check=False)
+    return outfile
+
+
+if __name__ == "__main__":
+    cfg_dir = Path("config_files")
+    static_ancillary_dir = Path("static_ancillary_files")
+    # Rosamond, track 01:
+    frame_id = 150
+    frequency = 'frequencyA'
+    polarization = 'HH'
+    reference_json = "opera-disp-nisar-reference-dates-dummy.json"
+    frame_to_bounds_json = "Frame_to_bounds_DISP-NI_v0.1.json"
+    # Creates one file for the forward mode and one for the historical mode.
+    for mode in ProcessingMode:
+        output_directory = Path(f"output/{mode.value}")
+        # TODO: adjust the number of
+        # ionosphere files
+        # troposphere files
+        dolphin_cfg_file = setup_delivery(cfg_dir=cfg_dir, mode=mode)
+        # Run the "convert_config.py" script in the same directory
+        # as this script.
+        this_dir = Path(__file__).parent
+        convert_config = this_dir / "convert_config.py"
+        arg_string = (
+            f" --frame-id {frame_id} "
+            f" --frequency {frequency} "
+            f" --polarization {polarization} "
+            f" --reference_date_database_json {static_ancillary_dir}/{reference_json} "
+            f" --frame-to-bounds-json {static_ancillary_dir}/{frame_to_bounds_json} "
+            f" --output-directory {output_directory}"
+            f" --processing-mode {mode.value} --save-compressed-slc -o"
+            f" {cfg_dir}/runconfig_{mode.value}.yaml  -a"
+            f" {cfg_dir}/algorithm_parameters_{mode.value}.yaml"
+        )
+        cmd = f"python {convert_config} {dolphin_cfg_file} {arg_string}"
+        print(cmd)
+        subprocess.run(cmd, shell=True, check=False)
+        # Remove the `dolphin` yamls
+        for f in cfg_dir.glob("dolphin_config*.yaml"):
+            f.unlink()
